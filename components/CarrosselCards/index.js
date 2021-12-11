0becd9283baff02275cf1df5334f8fc04dc30592
Jully Nixon <jully.niixon@gmail.com>
2021-12-11 09:31:15 -0300

    Carrossel de livros integrado com o backend.

--- components/CarrosselCards/index.js
+++ components/CarrosselCards/index.js
@@ -7,63 +7,101 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const CarrosselCards = ({ urlApi }) => {
-  const [data, setData] = useState([]);
+  const [dataAnuncios, setDataAnuncios] = useState([]);
+  const [dataDoacoes, setDataDoacoes] = useState([]);
+  const [dataUltimosAdicionados, setDataUltimosAdicionados] = useState([]);
+  const [dataProximos, setDataProximos] = useState([]);
+  const [dataDestaques, setDataDestaques] = useState([]);
 
   useEffect(() => {
-    if (!data.length) {
-      getAnuncios()
+    if (!dataAnuncios.length) {
+      getAnuncios();
     }
   });
 
   const getAnuncios = async () => {
     try {
       const resp = await axios.get(urlApi);
-      setData(resp.data)
+      setDataAnuncios(resp.data)
+      if (resp.data) {
+        getDoacoes(resp.data);
+        getProximos(resp.data);
+        getUltimosAdicionados(resp.data);
+        getDestaques(resp.data);
+      }
     } catch (err) {
       console.error(err);
     }
   }
 
+  const getDoacoes = (data) => {
+    const filtroDoacoes = data.filter(item => item.valor === 0);
+    setDataDoacoes(filtroDoacoes);
+  }
+
+  const getUltimosAdicionados = (data) => {
+    const filterUltimosAdicionados = data.filter(item => item.ano === 2017);
+    setDataUltimosAdicionados(filterUltimosAdicionados);
+  }
+
+  const getDestaques = (data) => {
+    const filterDestaques = data.filter(item => item.destaque === true);
+    setDataDestaques(filterDestaques);
+  }
+
+  const getProximos = (data) => {
+    const filterProximos = data.filter(item => item.instituicao === "UNICAP");
+    setDataProximos(filterProximos);
+  }
+
   return (
     <View>
       <Carousel
-        data={data}
+        data={dataDestaques}
         renderItem={CarrosselDestaque}
         sliderWidth={SLIDER_WIDTH_DESTAQUE}
         itemWidth={ITEM_WIDTH_DESTAQUE}
         useScrollView={true}
+        loop={true}
+        enableMomentum={false}
+        lockScrollWhileSnapping={true}
+        autoplayInterval={5000}
       />
       <Text style={styles.title}>Próximos a você</Text>
       <Carousel
-        data={data}
+        data={dataProximos}
         renderItem={CarrosselCardItem}
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
         useScrollView={true}
+        loop={true}
       />
       <Text style={styles.title}>Doações</Text>
       <Carousel
-        data={data}
+        data={dataDoacoes}
         renderItem={CarrosselCardItem}
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
         useScrollView={true}
+        loop={true}
       />
       <Text style={styles.title}>Últimos adicionados</Text>
       <Carousel
-        data={data}
+        data={dataUltimosAdicionados}
         renderItem={CarrosselCardItem}
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
         useScrollView={true}
+        loop={true}
       />
       <Text style={styles.title}>Em alta</Text>
       <Carousel
-        data={data}
+        data={dataAnuncios}
         renderItem={CarrosselCardItem}
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
         useScrollView={true}
+        loop={true}
       />
     </View>
 
