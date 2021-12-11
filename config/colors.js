759f1e8479ff2c18556d1185ba5910bc624ec4d1
Marilia Vilar <mariliavilarpr@gmail.com>
2021-12-11 13:17:53 -0300

    feature: inclusao de header customizado com logo, e colors de teste na tela iício

--- config/colors.js
+++ config/colors.js
@@ -8,4 +8,23 @@ export const colors = {
 	medium: "#6e6969",
 	dark: "#0c0c0c",
 	danger: "#ff5252",
+
+	mariliaGreenLight:"#BAD9C8",
+	mariliaGreen:"#95BFA4",
+	mariliaGreenDark:"#435944",
+	mariliaAccentPink:"#F27D72",
+	mariliaAccentOrange:"#F29966",
+
+	marilia2Background:"#D9CA9C",
+	marilia2Green:"##A3A656",
+	marilia2GreenAccent:"#81A64B",
+	marilia2Salmon:"#D96B43",
+	marilia2Pink:"#D9564A",
+
+	marilia3Background: "#F2E2C4",
+	marilia3Purple: "#8C4D6B",
+	marilia3OrangeLight: "#F28705",
+	marilia3OrangeDark:"#F28705",
+	marilia3OrangeAccent:"#F28705",
+
 };
