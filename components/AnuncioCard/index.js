e7dc8b2c2c28a417d52694c4d8f5371edb751a63
Jully Nixon <jully.niixon@gmail.com>
2021-12-12 13:36:20 -0300

    Correção visualização da lista de meus anúncios.

--- components/AnuncioCard/index.js
+++ components/AnuncioCard/index.js
@@ -1,34 +1,79 @@
 import React from 'react'
 import { View, Text, StyleSheet, FlatList } from "react-native"
-import AnunciosCardItem from '../../AnunciosCardItem'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+const dataMock = [
+  {
+    "anuncioStatus": "DISPONIVEL",
+    "criadoEm": "2021-10-05",
+    "descricao": "Melhor preço",
+    "destaque": null,
+    "fotoLivro": "https://firebasestorage.googleapis.com/v0/b/quarta-capa.appspot.com/o/qu%C3%ADmica.jpeg?alt=media&token=79093e8d-3a27-41af-bed8-57a1425b1376",
+    "id": "87710264-a002-4ca7-8462-d9236089833e",
+    "livro": {
+      "ano": 2014,
+      "anoEscolar": "PRIMEIRO_ANO_MEDIO",
+      "autor": "João Usberco",
+      "descricaoEstado": "USADO",
+      "disciplina": {
+        "id": "b96e26fd-cac1-49be-8233-6307217876b6",
+        "nome": "Química",
+      },
+      "disponivelParaDoacao": false,
+      "editora": "Saraiva Didáticos",
+      "id": "46582a9a-64c3-45d7-a132-2bbcaeef8505",
+      "isbn": "8502223291",
+      "titulo": "Química ",
+      "valor": 150,
+    },
+    "titulo": "Química - Volume único",
+    "usuario": {
+      "cpf": "58462031087",
+      "email": "Jully@gmail.com",
+      "id": "0593d811-10fc-4a4e-97a4-67a6647000e2",
+      "instituicao": null,
+      "nome": "Jully",
+      "numeroCelular": "5581991318758",
+    }
+  }
+];
+
 const AnunciosCard = ({ urlApi }) => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        if (!data.length) {
-          getAnuncios()
-        }
-      });
+  useEffect(() => {
+    console.log("useEffect");
+    // getAnuncios();
+  }, [null]);
 
-      const getAnuncios = async () => {
-        try {
-          const resp = await axios.get(urlApi);
-          setData(resp.data)
-        } catch (err) {
-          console.error(err);
-        }
-      }
+  const getAnuncios = async () => {
+    try {
+      const resp = await axios.get(urlApi);
+      setData(resp.data)
+      console.log(resp.data)
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-      return (
-        <View>
-          <FlatList
-            data={data}
-            renderItem={AnunciosCardItem}
-        />
-        </View>
+  const Item = ({ titulo, anoEscolar, valor }) => (
+    <View>
+      <Text>{titulo}</Text>
+      <Text>{anoEscolar}</Text>
+      <Text>{valor}</Text>
+    </View>
+  );
+
+  return (
+    <View>
+      <FlatList
+        data={dataMock}
+        renderItem={({ item }) => <Item titulo={item.titulo}
+          anoEscolar={item.livro.anoEscolar} valor={item.livro.valor} />}
+        keyExtractor={item => item.id}
+      />
+    </View>
   )
 }
 
